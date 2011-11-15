af2aa4ee9a63c853640fd51e73d2f6a6c19abadd
Nadav Tzur <nabadabav@gmail.com>
2011-11-15 22:59:09 +0200

    "Ass1/\327\224\327\225\327\250\327\223.htm"

--- Ass1/mergelog.cpp
+++ Ass1/mergelog.cpp
@@ -1,4 +1,5 @@
-//update nov.15.11 01:50
+//Nadav Tzur 038087169
+//Shlomi Israel 038070967
 
 #include <iostream>
 #include <vector>
@@ -11,12 +12,14 @@ public:
   string level;
   string timestamp;
   string message;
+  //default constructor
   Line()
   {
     this->level = "";
     this->timestamp = "";
     this->message = "";
   }
+  //constructor - constructs a new Line with the same data from l
   Line(const Line *l)
   {
     this->level = l->level;
@@ -30,11 +33,14 @@ class LineList
   public:
     Line* line;
     LineList* next;
+    //default constructor
     LineList()
     {
       this->line = NULL;
       this->next = NULL;
     }
+    
+    //returns a (deep) copy of the list 
     
     LineList* getCopy()
     {
@@ -69,69 +75,12 @@ void printLines(LineList* l);
 bool isLater(Line* a, Line* b);
 //returns true if LineList is empty
 bool isEmpty(LineList* l);
+//inserts a copy of `next` after the `prev` node.
+void insertCopy(LineList *prev , LineList *next);
+//checks if `a` is later than `b`
+bool isLater(Line* a, Line* b);
 
-LineList* mergeListsOld(LineList* resultList , LineList* mergedList)
-{
-  
-  if (isEmpty(mergedList) )
-    return resultList;
-  if (isEmpty(resultList) )
-  {
-    resultList=new LineList();
-    resultList->line=new Line(mergedList->line);
-    mergedList=mergedList->next;
-  }
-  LineList *iterator = resultList;
-  cout<<"lists not empty"<<endl;
-  while (iterator->next != NULL && mergedList != NULL)
-  {
-      cout<<"checks same value"<<endl;
-      cout<<"next: "<<iterator->next<<endl;
-      cout<<" this->timestamp: "<<iterator->line<<endl;
-      cout<<" next->timestamp: "<<iterator->next->line->timestamp<<endl;
-    while( iterator->next != NULL && iterator->line->timestamp == iterator->next->line->timestamp)
-    {
-      cout<<"is the same"<<endl;
 
-      iterator = iterator->next;
-    }
-    cout<<"node found"<<endl;
-
-    bool b = isLater(iterator->line , mergedList->line);
-    if (b == true)
-    {
-      LineList* tmp = new LineList();
-      
-      tmp->line = new Line(mergedList->line);
-      tmp->next = iterator->next;
-      iterator->next = tmp;
-      iterator = iterator->next;
-      mergedList = mergedList->next;      
-    }
-  }
-  
-  //return the resultList if we finisht the Lines in the mergedList
-  if(mergedList== NULL)
-    return resultList;
- 
-
-  //copy the rest of the mergedList if we at the end of the resultList
-  
-  while(mergedList != NULL)
-  {
-    LineList* rest = new LineList();
-    rest->line = new Line(mergedList->line);
-    iterator->next = rest;
-    iterator = iterator->next;
-    mergedList = mergedList->next;
-    
-  }
-  
-  //the next of the end of the resultList will point to NULL;
-  iterator->next = NULL;
-  return resultList;
-
-}
 
 void insertCopy(LineList *prev , LineList *next)
 {
@@ -142,7 +91,14 @@ void insertCopy(LineList *prev , LineList *next)
   nextCopy->next = tmp;
 }
 
-
+/*
+ * mergeLists - the main idea, 
+ * the function merge two lists, one is the resultlist untik now, and adds the elements from
+ * the merged list in the proper place.
+ * we iterate on both lists, and returning the first element on the resultlist.
+ * if we put a new element before the first in thr resultlist, we change the first pointer as well.
+ * 
+ */
 
 LineList* mergeLists(LineList* resultList , LineList* mergedList)
 {
@@ -150,20 +106,20 @@ LineList* mergeLists(LineList* resultList , LineList* mergedList)
     LineList *merIterator = mergedList;
     LineList *resIterator = resultList;
     LineList *prevResIterator = NULL;
+    //basic cases
     if (resultList == NULL && mergedList == NULL)
       return NULL;
     if(resultList == NULL)
     {
       return mergedList->getCopy();
     }
-
+    //main loop
     while (resIterator != NULL && merIterator != NULL)
     {
-      cout<<"status res: "<<endl;
-      //printLines(resultList);
-      
+      //checks if the resIterator is later than the merIterator
       if(isLater(resIterator->line , merIterator->line))
       {
+	//checks if the iterator is in the beginning of the resultList
 	if(prevResIterator != NULL)
 	{
 	  insertCopy(prevResIterator, merIterator);
@@ -174,6 +130,7 @@ LineList* mergeLists(LineList* resultList , LineList* mergedList)
 	  prevResIterator=new LineList();
 	  prevResIterator->line=new Line(merIterator->line);
 	  prevResIterator->next=resIterator;
+	  resultList = prevResIterator;
 	}
 	merIterator = merIterator->next;
       }
@@ -183,17 +140,18 @@ LineList* mergeLists(LineList* resultList , LineList* mergedList)
 	resIterator = resIterator->next;
       }
     }
+    //copy the tail of the mergedList to the end of the result list
     if(merIterator!=NULL)
       prevResIterator->next = merIterator->getCopy();
     return resultList;
 }
 
-
+//seperate the parts of `line` and returns a new Line object.
 Line* parseLine(string line)
 {
   Line* parse = new Line();
   vector<string> victor;
-  int i;
+  unsigned int i=0;
   string str="";
   for(i=0 ; i<line.length() ; i++)
   {
@@ -214,7 +172,7 @@ Line* parseLine(string line)
   return parse;
 }
 
-
+//prints the lines
 void printLines(LineList* l)
 {
   LineList* iterator = l;
@@ -224,28 +182,24 @@ void printLines(LineList* l)
     iterator = iterator->next;
   }
 }
-
+//checks if a later then b , using character values 
 bool isLater(Line* a, Line* b)
 {
-  int i;
-  cout<<"checking if "<<a->timestamp<<">"<<b->timestamp<<"...";
+  unsigned int i;
   for ( i = 0; i<a->timestamp.length() ; i++)
   {
     if(a->timestamp[i] > b->timestamp[i])
     {
-      cout<<"yes"<<endl;
       return true;
     }
     else if(a->timestamp[i] < b->timestamp[i])
     {
-      cout<<"NOPE!"<<endl;
       return false;
     }
   }
-  cout<<"NOP"<<endl;
   return false;
 }
-
+//checks if the list is empty
 bool isEmpty(LineList* l)
 {
   return (l==NULL);
@@ -308,39 +262,44 @@ int main()
     string line;
     LineList *resultList=NULL, *mergedList=NULL;
     getline(cin, line);
-        cout<<"line: "<<line.length()<<endl;
-	if(line.length() == 0 || line[0] != '#')
-	{
-	  cout<<"ERROR"<<endl;
-	  eof = true;
-	}
-	else
-	{
-	  while (!eof)
-	  {	
-		if(isNewFile(line))
-		{
-		  resultList= mergeLists(resultList , mergedList);
-		  deleteList(mergedList);
-		  mergedList=NULL;
+    //checks if the filr starts with '#' or if the `line` in empty.
+    if(line.length() == 0 || line[0] != '#')
+    {
+      cout<<"ERROR"<<endl;
+      eof = true;
+    }
+    else
+    {
+      while (!eof)
+      {	
+	    //if we encounter a new file, than we need to merge the two lists. and afterwards deletes the entire `mergeList`
+	    if(isNewFile(line))
+	    {
+	      resultList= mergeLists(resultList , mergedList);
+	      deleteList(mergedList);
+	      mergedList=NULL;
 
-		}
-		else
-		{
-		  Line *l = parseLine(line);
-		  mergedList= addOneLine(mergedList, l);
-		}
-		getline(cin, line);
-		if (cin.eof())
-		  eof = true;
-		
-	  }
-	  cout<<"before const final merge"<<endl;
-	  resultList = mergeLists(resultList , mergedList);
-	  cout<<"after final merge"<<endl;
+	    }
+	    //otherwise we simply add a line to `mergeList`
+	    else
+	    {
+	      Line *l = parseLine(line);
+	      mergedList= addOneLine(mergedList, l);
+	    }
+	    getline(cin, line);
+	    if (cin.eof())
+	      eof = true;
+	    
+      }
+      //merge one last time for the last created mergelist (there is no more '#')
+      resultList = mergeLists(resultList , mergedList);
 
-	  printLines(resultList);
-	  deleteList(resultList);
-	  deleteList(mergedList);
-	}
+      //prints the output
+      printLines(resultList);
+      
+      //frees all heap blocks.
+      deleteList(resultList);
+      deleteList(mergedList);
+    }
+    return 0;
 }
