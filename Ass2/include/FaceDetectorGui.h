dfdb66030eed0e28c387d46517d2d9d6005bc140
Nadav Tzur <nabadabav@gmail.com>
2011-11-24 00:45:36 +0200

    adding FaceDetectorGui h and cpp

---
+++ Ass2/include/FaceDetectorGui.h
@@ -0,0 +1,36 @@
+
+ * FaceDetectorGui.h
+ *
+ *  Created on: Nov 22, 2011
+ *      Author: shlomi
+ * 	Author: nadavi
+ */
+
+#ifndef FACEDETECTORGUI_H_
+#define FACEDETECTORGUI_H_
+
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+
+using namespace cv;
+using namespace std:
+
+class FaceDetectorGui
+{
+  private:
+    //FaceDetector an object that detects faces into a rectangles.
+    FaceDetector detector;
+    string linuxTitle;
+    vector<Rect> faces;
+    Mat result;
+  public:
+    FaceDetectorGui(const string &imgName ,const string &linuxTitle);
+    bool show();
+    bool save(const string &targetFile);
+}:
+
+
+
+#endif /* FACEDETECTORGUI_H_ */
---
+++ Ass2/src/FaceDetectorGui.cpp
@@ -0,0 +1,32 @@
+
+
+#include "../include/FaceDetectorGui.h"
+
+ 
+FaceDetectorGui::FaceDetectorGui(const string &imgName ,const string &linuxTitle) : detector(imgname), imgTitle(linuxTitle)
+{
+  this->faces = detector.getFaces();
+  this->result = imread(imgName);
+}
+
+bool FaceDetectorGui::show()
+{
+  int i;
+  if(this->result.empty())
+    return false;
+  for ( i = 0 ; i<this->faces.size() ; i++ )
+  {
+    Point p1 (this->faces[i].x , this->faces[i].y);
+    Point p2 (this->faces[i].x + this->faces[i].width , this->faces[i].y + this->faces[i].height);
+    rectangle(this->result, p1, p2, Scalar(255 , 0 , 255) , 200);
+  }
+  
+  imshow(this->imgTitle , this->result);
+  return true;
+}
+
+
+bool FaceDetectorGui::save(const string &targetFile)
+{
+  imwrite(targetFile , this->result);
+}
