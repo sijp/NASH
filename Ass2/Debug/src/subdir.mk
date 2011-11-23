390e877b67740dd9b2268dc7cd4641db0545edd0
Shlomi Israely <sijproject@gmail.com>
2011-11-23 11:45:32 +0200

    Assignment 2

---
+++ Ass2/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/FaceDetector.cpp \
+../src/step1.cpp 
+
+OBJS += \
+./src/FaceDetector.o \
+./src/step1.o 
+
+CPP_DEPS += \
+./src/FaceDetector.d \
+./src/step1.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I/usr/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Ass2/include/FaceDetector.h
@@ -0,0 +1,31 @@
+/*
+ * FaceDetector.h
+ *
+ *  Created on: Nov 22, 2011
+ *      Author: shlomi
+ */
+
+#ifndef FACEDETECTOR_H_
+#define FACEDETECTOR_H_
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+using namespace std;
+using namespace cv;
+
+class FaceDetector
+{
+	private:
+		Mat image;
+	public:
+		//read the image into a cv::mat object.
+		FaceDetector(const string &imgName);
+		vector<Rect> getFaces();
+};
+
+
+
+
+#endif /* FACEDETECTOR_H_ */
