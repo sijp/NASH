ebdc672380524a106f4da9a68759978c6f4a7278
Nadav Tzur <nabadabav@gmail.com>
2011-12-31 12:59:47 +0200

    WarSim AKA main

---
+++ Ass3/src/nash/ass3/WarSim.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package nash.ass3;
+
+import java.io.IOException;
+
+/**
+ * @author nadav , shlomi
+ *
+ */
+public class WarSim {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+		BoardImpl.getInstance().readProperties(args[0]);
+		ChiefOfStaffImpl.getInstance().readProperties(args[1]);
+		WarehouseImpl.getInstance().readProperties(args[2]);
+		}
+		catch (IOException ioe)
+		{
+			System.out.println(ioe.getMessage());
+		}
+
+	}
+
+}
