fbc33091380c3a99b7bfe17c265189132e6b27bf
Nadav Tzur <nabadabav@gmail.com>
2012-01-27 15:17:52 +0200

    adding main

---
+++ Ass4/Ass4/Manager/src/nash/ass4/Run.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package nash.ass4;
+
+/**
+ * @author nadav
+ *
+ */
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		int port = Integer.decode(args[0]).intValue();
+		MultipleClientProtocolServer server=new MultipleClientProtocolServer(port, new HttpProtocolFactory());
+		Thread serverThread = new Thread(server);
+		serverThread.start();
+		try{
+			serverThread.join();
+		}catch(InterruptedException ie)
+		{
+			System.out.println("Server has stopped");
+		}
+	}
+
+}
